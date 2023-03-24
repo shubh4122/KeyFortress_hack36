d4ac3fc0a8c19883fedb852f9b1b829d3c5d9af1
mishra_abhi <abhi200108010@gmail.com>
2023-03-25 01:34:10 +0530

    added user class and gms dependency

---
+++ app/src/main/java/com/android/keyfortress_hack36/auth/GoogleSignIn.java
@@ -0,0 +1,17 @@
+package com.android.keyfortress_hack36.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.keyfortress_hack36.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class GoogleSignIn extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_sign_in);
+
+    }
+}
---
+++ app/src/main/java/com/android/keyfortress_hack36/model/User.java
@@ -0,0 +1,18 @@
+package com.android.keyfortress_hack36.model;
+
+public class User {
+    private final String name, email;
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
